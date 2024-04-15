f46bdbc63aaf66bbdcfeb651145c8f7be1ff1f96
ridhwanridh <137137909+ridhwanridh@users.noreply.github.com>
2024-04-14 21:46:52 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = '26d3742927cf42afaa203724241504'; // this is my api key
+
+    // API endpoints for London and New York
+    const apiUrlLondon = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London&aqi=no`;
+	const apiUrlNewYork = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=New%20York&aqi=no`;
+
+
+
+    function fetchWeather(apiUrl, locationName) {
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+
+                const weatherInfoElement = document.getElementById('weather-info');
+                const weatherCard = document.createElement('div');
+                weatherCard.classList.add('weather-card');
+
+                const weatherData = `
+                    <p><strong>Location:</strong> ${locationName}</p>
+                    <p><strong>Temperature:</strong> ${data.current.temp_c}°C</p>
+                    <p><strong>Condition:</strong> ${data.current.condition.text}</p>
+                `;
+
+                weatherCard.innerHTML = weatherData;
+
+                weatherInfoElement.appendChild(weatherCard);
+            })
+            .catch(error => {
+                console.error(`Error fetching weather data for ${locationName}:`, error);
+            });
+    }
+
+    fetchWeather(apiUrlLondon, 'London');
+
+    fetchWeather(apiUrlNewYork, 'New York');
+});
